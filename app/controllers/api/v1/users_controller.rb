af70976347a3a12aaac4e4701da37e3f60141089
Amy Camacho <amyamyc@hotmail.com>
2023-07-02 13:29:17 -0300

    Adds update and destroy action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,10 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :set_user, only: %i[show update destroy]
+
       def show
-        render json: User.find(params[:id])
+        render json: @user
       end
 
       def create
@@ -14,12 +16,28 @@ module Api
         end
       end
 
+      def update
+        if @user.update(user_params)
+          render json: @user, status: :ok
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user.destroy
+        head 204
+      end
+
       private
 
       def user_params
         params.require(:user).permit(:email, :password)
       end
 
+      def set_user
+        @user = User.find(params[:id])
+      end
     end
   end
 end
