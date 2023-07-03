75619de3ec35f31a28d4c72fd1ca2a12840e3376
Amy Camacho <amyamyc@hotmail.com>
2023-07-03 20:09:20 -0300

    Adds validations to donation model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,3 +1,13 @@
 class Donation < ApplicationRecord
   belongs_to :user
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :payment_method, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  after_create :send_thank_you_email
+
+  def send_thank_you_email
+    DonationMailer.thank_you_email(self).deliver_now
+  end
 end
--- test/models/donation_test.rb
+++ test/models/donation_test.rb
@@ -1,7 +1,67 @@
-require "test_helper"
+require 'test_helper'
 
 class DonationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid with valid attributes' do
+    donation = Donation.new(
+      amount: 100.0,
+      payment_method: 1,
+      email: 'test@example.com',
+      user: users(:one) # Assumes you have a fixture or factory for users
+    )
+    assert donation.valid?
+  end
+
+  test 'should be invalid without an amount' do
+    donation = Donation.new(
+      payment_method: 1,
+      email: 'test@example.com',
+      user: users(:one)
+    )
+    assert_not donation.valid?
+    assert_includes donation.errors[:amount], "can't be blank"
+  end
+
+  test 'should be invalid without a payment method' do
+    donation = Donation.new(
+      amount: 100.0,
+      email: 'test@example.com',
+      user: users(:one)
+    )
+    assert_not donation.valid?
+    assert_includes donation.errors[:payment_method], "can't be blank"
+  end
+
+  test 'should be invalid without an email' do
+    donation = Donation.new(
+      amount: 100.0,
+      payment_method: 1,
+      user: users(:one)
+    )
+    assert_not donation.valid?
+    assert_includes donation.errors[:email], "can't be blank"
+  end
+
+  test 'should be invalid with an invalid email format' do
+    donation = Donation.new(
+      amount: 100.0,
+      payment_method: 1,
+      email: 'invalid_email',
+      user: users(:one)
+    )
+    assert_not donation.valid?
+    assert_includes donation.errors[:email], 'is invalid'
+  end
+
+  test 'should send thank you email after creation' do
+    donation = Donation.create(
+      amount: 100.0,
+      payment_method: 1,
+      email: 'test@example.com',
+      user: users(:one)
+    )
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_equal 'Thank you for your donation!', ActionMailer::Base.deliveries.last.subject
+    assert_equal 'test@example.com', ActionMailer::Base.deliveries.last.to[0]
+  end
 end
