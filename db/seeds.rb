c799ad908750ca46d962f290c879faab010de37e
Amy Camacho <amyamyc@hotmail.com>
2023-07-04 19:10:19 -0300

    Create a seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do
+  user = User.create!(email: Faker::Internet.email, password: 'locadex1234')
+  puts "Created a new user: #{user.email}"
+
+  2.times do
+    donation = Donation.create!(
+      amount: rand(1.0..100.0),
+      payment_method: rand(1..3),
+      email: user.email,
+      user_agent: Faker::Internet.user_agent,
+      ip_address: Faker::Internet.ip_v4_address,
+      user_id: user.id
+    )
+    puts "Created a brand new donation with amount #{donation.amount}"
+  end
+end
